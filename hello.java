3b487767fe7f60c74eee06aacc1d134e8eb57a80
Deep12cr <deepakazm2810@gmail.com>
2024-07-31 10:54:36 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+// Define a public class named HelloWorld
+public class hello {
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
